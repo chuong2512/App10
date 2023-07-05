2dcc2f6e7b4d1e97f30cf1a311a7ba80f54bfab8
chuong_gamer <hieutg244@gmail.com>
2023-07-05 20:27:42 +0700

    adsad

---
+++ Assets/Trang phuc bieu dien/Scripts/Shop/ButtonRegister.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonRegister : MonoBehaviour
+{
+    public int number;
+    public TextMeshProUGUI TextMeshProUgui;
+    public Button button;
+
+    void OnValidate()
+    {
+        TextMeshProUgui = GetComponentInChildren<TextMeshProUGUI>();
+        button = GetComponentInChildren<Button>();
+    }
+//////////////////////
+
+    private void SetTextDay()
+    {
+        switch (number)
+        {//////////////////////
+            case 1:
+                TextMeshProUgui.SetText("Mua thêm 1 ngày");
+                break;
+            case 2://////////////////////
+                TextMeshProUgui.SetText("Mua thêm 7 ngày");
+                break;
+            case 3:
+                TextMeshProUgui.SetText("Mua thêm 30 ngày");
+                break;
+            case 4:
+                TextMeshProUgui.SetText("Mua thêm 90 ngày");
+                break;
+            case 5:
+                TextMeshProUgui.SetText("Mua thêm 180 ngày");
+                break;
+        }
+    }//////////////////////
+
+    void Start()
+    {
+        button.onClick.AddListener(Choose);
+
+        SetTextDay();
+    }
+
+    private void Choose()
+    {
+        RegisterManager.Instance.OnPressDown(number);
+    }
+}
